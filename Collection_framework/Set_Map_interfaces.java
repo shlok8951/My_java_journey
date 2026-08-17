dfad8e09d9a02b30457b95d6ad8f9f2f94690f30
shlok8951 <shlok771@gmail.com>
2026-08-17 23:09:21 +0530

    add constructer types of Set/Map

--- Collection_framework/Set_Map_interfaces.java
+++ Collection_framework/Set_Map_interfaces.java
@@ -7,6 +7,7 @@ public class Set_Map_interfaces {
         set.add("Aditya");
         set.add("Rohit");
         set.add("Mohit");
+
         //--------- contains(Object o)->return boolean 
         System.out.println(set.contains("Rohit"));
 
@@ -19,6 +20,21 @@ public class Set_Map_interfaces {
         System.out.println(map.containsKey(101));
         System.out.println(map.get(103));
         
+        //------------------------- Constructors of HashSet and LinkedHashSet
+        //  LinkedHshSet extends the HashSet
+        //Linked HashSet similar Construc same Hashset.
         
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>(100);
+        Set<Integer> set3 = new HashSet<>(100,0.8f);
+        Set<Integer> set4 = new HashSet<>(List.of(10,20,30,40));
+
+        //---------------- Constructors of HashMap and Linked Hashmap
+        //LinkedHashMap similar it
+        Map<Integer,String> map3 = new HashMap<>();
+        Map<Integer,String> map4 = new HashMap<>(100);
+        Map<Integer,String> map5 = new HashMap<>(100,0.8f);
+        Map<Integer,String> map6 = new HashMap<>(Map.of(101,"Akash",102,"hi",103,"hello"));
+         
     }
 }
